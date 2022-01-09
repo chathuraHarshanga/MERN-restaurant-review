3f8cbbf3f2564c47642a1cfbe4047a7541c6ad3a
Chathura Harshanga <38973288+chathuraHarshanga@users.noreply.github.com>
2022-01-09 21:05:05 +0530

    mongodb connection

--- backend/api/restaurants.routes.js
+++ backend/api/restaurants.routes.js
@@ -1,5 +1,5 @@
 import express from "express";
-
+import restaurentsCtrl from "../doa/restaurantsDAO.js";
 const router = express.Router();
 
 router.route("/").get((req, res) => res.send("hello world"));
